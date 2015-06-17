8b08f5dfe2a7fbfb36696c787a6df4d55560f1f1
Franco Pellegrini <francogpellegrini@gmail.com>
2015-06-17 20:35:35 -0300

    Nuevo experimento

---
+++ src/ar/edu/unrc/game2048/performanceandtraining/configurations/ntuples/BasicScoreNoPartialTanH.java
@@ -0,0 +1,247 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.edu.unrc.game2048.performanceandtraining.configurations.ntuples;
+
+import ar.edu.unrc.game2048.Game2048;
+import ar.edu.unrc.game2048.GameBoard;
+import ar.edu.unrc.game2048.NTupleConfiguration2048;
+import ar.edu.unrc.game2048.Tile;
+import ar.edu.unrc.tdlearning.perceptron.learning.FunctionUtils;
+import ar.edu.unrc.tdlearning.perceptron.ntuple.SamplePointState;
+import java.util.ArrayList;
+import org.encog.util.arrayutil.NormalizationAction;
+import org.encog.util.arrayutil.NormalizedField;
+
+/**
+ *
+ * @author Franco
+ */
+public class BasicScoreNoPartialTanH extends NTupleConfiguration2048 {
+
+    int maxReward = 500_000;
+    int minReward = -500_000;
+
+    /**
+     *
+     */
+    public BasicScoreNoPartialTanH() {
+        this.activationFunction = FunctionUtils.tanh;
+        this.derivatedActivationFunction = FunctionUtils.derivatedTanh;
+        double activationFunctionMax = 1;
+        double activationFunctionMin = -1;
+
+        normOutput = new NormalizedField(NormalizationAction.Normalize,
+                null, maxReward, minReward, activationFunctionMax, activationFunctionMin);
+
+        nTuplesLenght = new int[17];
+        for ( int i = 0; i < 17; i++ ) {
+            nTuplesLenght[i] = 4;
+        }
+
+        int maxTile = 15;
+        this.allSamplePointStates = new ArrayList<>();
+        for ( int i = 0; i <= maxTile; i++ ) {
+            allSamplePointStates.add(new Tile(i));
+        }
+    }
+
+    /**
+     *
+     * @param value <p>
+     * @return
+     */
+    @Override
+    public double denormalizeValueFromPerceptronOutput(Object value) {
+        return normOutput.deNormalize((double) value);
+    }
+
+    /**
+     *
+     * @param board
+     * @param outputNeuron <p>
+     * @return
+     */
+    @Override
+    public double getBoardReward(GameBoard board, int outputNeuron) {
+        return 0;
+    }
+
+    @Override
+    public double getFinalReward(Game2048 game, int outputNeuron) {
+        return game.getScore();
+    }
+
+    /**
+     *
+     * @param board
+     * @param nTupleIndex <p>
+     * @return
+     */
+    @Override
+    public SamplePointState[] getNTuple(GameBoard board, int nTupleIndex) {
+        switch ( nTupleIndex ) {
+            // verticales
+            case 0: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 0),
+                            board.tileAt(0, 1),
+                            board.tileAt(0, 2),
+                            board.tileAt(0, 3)};
+                return sample;
+            }
+            case 1: {
+                SamplePointState[] sample
+                        = {board.tileAt(1, 0),
+                            board.tileAt(1, 1),
+                            board.tileAt(1, 2),
+                            board.tileAt(1, 3)};
+                return sample;
+            }
+            case 2: {
+                SamplePointState[] sample
+                        = {board.tileAt(2, 0),
+                            board.tileAt(2, 1),
+                            board.tileAt(2, 2),
+                            board.tileAt(2, 3)};
+                return sample;
+            }
+            case 3: {
+                SamplePointState[] sample
+                        = {board.tileAt(3, 0),
+                            board.tileAt(3, 1),
+                            board.tileAt(3, 2),
+                            board.tileAt(3, 3)};
+                return sample;
+            }
+            // horizontales
+            case 4: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 0),
+                            board.tileAt(1, 0),
+                            board.tileAt(2, 0),
+                            board.tileAt(3, 0)};
+                return sample;
+            }
+            case 5: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 1),
+                            board.tileAt(1, 1),
+                            board.tileAt(2, 1),
+                            board.tileAt(3, 1)};
+                return sample;
+            }
+            case 6: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 2),
+                            board.tileAt(1, 2),
+                            board.tileAt(2, 2),
+                            board.tileAt(3, 2)};
+                return sample;
+            }
+            case 7: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 3),
+                            board.tileAt(1, 3),
+                            board.tileAt(2, 3),
+                            board.tileAt(3, 3)};
+                return sample;
+            }
+            // cuadrados
+            // primera fila de rectangulos
+            case 8: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 0),
+                            board.tileAt(0, 1),
+                            board.tileAt(1, 1),
+                            board.tileAt(1, 0)};
+                return sample;
+            }
+            case 9: {
+                SamplePointState[] sample
+                        = {board.tileAt(1, 0),
+                            board.tileAt(1, 1),
+                            board.tileAt(2, 1),
+                            board.tileAt(2, 0)};
+                return sample;
+            }
+            case 10: {
+                SamplePointState[] sample
+                        = {board.tileAt(2, 0),
+                            board.tileAt(2, 1),
+                            board.tileAt(3, 1),
+                            board.tileAt(3, 0)};
+                return sample;
+            }
+            //segunda fila de rectangulos
+            case 11: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 1),
+                            board.tileAt(0, 2),
+                            board.tileAt(1, 2),
+                            board.tileAt(1, 1)};
+                return sample;
+            }
+            case 12: {
+                SamplePointState[] sample
+                        = {board.tileAt(1, 1),
+                            board.tileAt(1, 2),
+                            board.tileAt(2, 2),
+                            board.tileAt(2, 1)};
+                return sample;
+            }
+            case 13: {
+                SamplePointState[] sample
+                        = {board.tileAt(2, 1),
+                            board.tileAt(2, 2),
+                            board.tileAt(3, 2),
+                            board.tileAt(3, 1)};
+                return sample;
+            }
+            //tercera fila de rectangulos
+            case 14: {
+                SamplePointState[] sample
+                        = {board.tileAt(0, 2),
+                            board.tileAt(0, 3),
+                            board.tileAt(1, 3),
+                            board.tileAt(1, 2)};
+                return sample;
+            }
+            case 15: {
+                SamplePointState[] sample
+                        = {board.tileAt(1, 2),
+                            board.tileAt(1, 3),
+                            board.tileAt(2, 3),
+                            board.tileAt(2, 2)};
+                return sample;
+            }
+            case 16: {
+                SamplePointState[] sample
+                        = {board.tileAt(2, 2),
+                            board.tileAt(2, 3),
+                            board.tileAt(3, 3),
+                            board.tileAt(3, 2)};
+                return sample;
+            }
+
+            default: {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        }
+    }
+
+    /**
+     *
+     * @param value <p>
+     * @return
+     */
+    @Override
+    public double normalizeValueToPerceptronOutput(Object value) {
+        if ( (Double) value > maxReward ) {
+            throw new IllegalArgumentException("value no puede ser mayor a maxReward=" + maxReward);
+        }
+        return normOutput.normalize((Double) value);
+    }
+}
