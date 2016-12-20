972cf9c2cb024a0380cfd84264028a544afc8675
Franco Pellegrini <francogpellegrini@gmail.com>
2016-12-20 01:19:47 -0300

    Se muestra explorationRate en la salida por terminal al entrenar

--- src/main/java/ar/edu/unrc/game2048/experiments/configurations/LearningExperiment.java
+++ src/main/java/ar/edu/unrc/game2048/experiments/configurations/LearningExperiment.java
@@ -242,11 +242,18 @@ class LearningExperiment {
     }
 
     /**
-     * @param experimentName nombre del experimento.
+     * Establece el nombre del experimento basado en el nombre de la clase {@code experimentClass}.
+     *
+     * @param experimentClass clase de la cual extraer el nombre del experimento.
      */
     public
-    void setExperimentName( String experimentName ) {
-        this.experimentName = experimentName;
+    void setExperimentName( Class experimentClass ) {
+        String className = experimentClass.getName();
+        int    lastDot   = className.lastIndexOf('.');
+        if ( lastDot != -1 ) {
+            className = className.substring(lastDot + 1);
+        }
+        experimentName = className;
     }
 
     /**
@@ -844,18 +851,11 @@ class LearningExperiment {
     }
 
     /**
-     * Establece el nombre del experimento basado en el nombre de la clase {@code experimentClass}.
-     *
-     * @param experimentClass clase de la cual extraer el nombre del experimento.
+     * @param experimentName nombre del experimento.
      */
     public
-    void setExperimentName( Class experimentClass ) {
-        String className = experimentClass.getName();
-        int    lastDot   = className.lastIndexOf('.');
-        if ( lastDot != -1 ) {
-            className = className.substring(lastDot + 1);
-        }
-        experimentName = className;
+    void setExperimentName( String experimentName ) {
+        this.experimentName = experimentName;
     }
 
     /**
@@ -1078,8 +1078,10 @@ class LearningExperiment {
                         .append(")")
                         .append("\tturno alcanzado = ")
                         .append(game.getLastTurn())
-                        .append("\tcurrent alpha = ")
+                        .append("\tcurrentAlpha = ")
                         .append(Arrays.toString(learningAlgorithm.getCurrentAlpha()))
+                        .append("\tcurrentExplorationRate = ")
+                        .append(learningAlgorithm.getCurrentExplorationRate())
                         .toString());
             } else {
                 System.out.println(new StringBuilder().append(( needToSaveBestGame ) ? "!! " : "")
@@ -1101,6 +1103,8 @@ class LearningExperiment {
                         .append(game.getLastTurn())
                         .append("\tcurrent alpha = ")
                         .append(Arrays.toString(learningAlgorithm.getCurrentAlpha()))
+                        .append("\tcurrentExplorationRate = ")
+                        .append(learningAlgorithm.getCurrentExplorationRate())
                         .toString());
             }
 
