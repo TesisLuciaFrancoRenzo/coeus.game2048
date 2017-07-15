112b95fba8081f7ca93e99f4de5d82bac52e29ed
Lucia Noelia Bressan <lucyluz333@gmail.com>
2017-07-15 18:02:05 -0300

    Agregado comentario para recordar de que se trataba este test

--- src/main/java/ar/edu/unrc/game2048/experiments/performance/NTupleSystemFuseTest.java
+++ src/main/java/ar/edu/unrc/game2048/experiments/performance/NTupleSystemFuseTest.java
@@ -9,8 +9,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * La colocamos como deprecated, porque los pocos experimientos que realizamos tarda mucho en obtener resultados.
+ * Lo que hacemos es entrenar diferentes cerebro, y luego los unimos, los clonamos y volvemos a entrenar.
+ *
  * @author lucia bressan, franco pellegrini, renzo bianchini
  */
+@Deprecated
 public
 class NTupleSystemFuseTest {
 
@@ -20,13 +24,15 @@ class NTupleSystemFuseTest {
         final ConfigNTupleBasicLinear_512 nTupleConfiguration = new ConfigNTupleBasicLinear_512();
         final int                         numFiles            = 7;
         final List< NTupleSystem >        nTupleSystems       = new ArrayList<>(numFiles);
-        final String dir =
+        final String                      dir                 =
                 "c:\\Users\\franc\\Trabajo\\Programas\\Gradle-Projects\\Tesis\\Perceptrones ENTRENADOS\\AutomaticTests\\";
-        final String dirEnd = "\\NTuple\\ConfigNTupleBasicLinear_512\\BasicLinear_512_trained.ser";
+        final String                      dirEnd              = "\\NTuple\\ConfigNTupleBasicLinear_512\\BasicLinear_512_trained.ser";
         for ( int i = 0; i < numFiles; i++ ) {
             nTupleSystems.add(new NTupleSystem(nTupleConfiguration.getAllSamplePointPossibleValues(),
                     nTupleConfiguration.getNTuplesLength(),
-                    nTupleConfiguration.getActivationFunction(), nTupleConfiguration.getDerivedActivationFunction(), false));
+                    nTupleConfiguration.getActivationFunction(),
+                    nTupleConfiguration.getDerivedActivationFunction(),
+                    false));
             nTupleSystems.get(i)
                     .load(new File(dir + i + "-alpha_0.005-anneal_2000000-lambda_0.0-gamma_1.0-explorationRate_0.1_0.01_0_500000-resetTraces_false" +
                                    dirEnd));
